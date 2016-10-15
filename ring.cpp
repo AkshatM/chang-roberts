585cf8881e4d3e2a46cfdb2e708224a671632a50
Akshat Mahajan <akshatm.bkk@gmail.com>
2016-10-15 14:29:19 -0700

    Update ring.cpp

--- ring.cpp
+++ ring.cpp
@@ -5,42 +5,37 @@
 #include <vector>
 
 class Node {
-
-	public:
-	    int identifier;
+    public:
+        int identifier;
         std::pair<int,int> message;
 
         // maintain a pointer to our neighbour, to avoid infinite recursion issues
         Node* neighbour;
 
         // our constructor
-		Node() {
+	Node() {
 
             // setup our identifier
-			this->identifier = rand(); // can only go upto 2^15, but that is fine
-
-			// setup our message
+	    this->identifier = rand(); // can only go upto 2^15, but that is fine
+		
+	    // setup our message
             this->message = std::make_pair(this->identifier, 0);
-		};
+	};
 
         void receive_message(std::pair<int,int> message) {
 
-			if (message.first == this->identifier) {
-
-                // boilerplate needed to print out our vector appropriately
-                std::cout << "[" << message.first << "," << message.second << "]" << std::endl;
-
-			} else {
-
-                message.second += 1;
-                this->send_message(message);
-
-			}
-		};
+	   if (message.first == this->identifier) {
+               // boilerplate needed to print out our vector appropriately
+               std::cout << "[" << message.first << "," << message.second << "]" << std::endl;
+	   } else {
+               message.second += 1;
+               this->send_message(message);
+	   }
+	};
 
         void send_message(std::pair<int, int> message) {
-			this->neighbour->receive_message(message);
-		};
+	    this->neighbour->receive_message(message);
+	};
 };
 
 int main() {
@@ -50,18 +45,18 @@ int main() {
     std::vector<Node> nodes;
 
     for ( auto i : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} ) { 
-		nodes.push_back(Node());
-	}
+	nodes.push_back(Node());
+    }
 
     for (int i = 0; i < 9; i++) {
-		nodes[i].neighbour = &nodes[i+1];
-	}
+	nodes[i].neighbour = &nodes[i+1];
+    }
 
     nodes[nodes.size() - 1].neighbour = &nodes[0];
- 
-    for (auto node : nodes) {
-		node.send_message(node.message);
-	}
 
-	return 0;
+    for (auto node : nodes) {
+        node.send_message(node.message);
+    }
+
+    return 0;
 }
